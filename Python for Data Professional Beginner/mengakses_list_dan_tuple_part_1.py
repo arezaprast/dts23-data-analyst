2b21aa0de481b697a5290f4833f9084bd6564ede
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-05 13:04:47 +0700

    Update file

---
+++ Python for Data Professional Beginner/mengakses_list_dan_tuple_part_1.py
@@ -0,0 +1,5 @@
+bulan_pembelian = ('januari', 'februari', 'maret', 'april', 'mei','juni', 'juli', 'agustus', 'september', 'oktober', 'november', 'desember')
+print(bulan_pembelian[0])
+print(bulan_pembelian[5])
+print(bulan_pembelian[-1])
+print(bulan_pembelian[-2])
