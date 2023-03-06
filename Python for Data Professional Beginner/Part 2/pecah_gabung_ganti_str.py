497cea4ef9311115b71f39f4d87dc4ddcc56d5fa
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-06 09:58:43 +0700

    Create pecah_gabung_ganti_str.py

---
+++ Python for Data Professional Beginner/Part 2/pecah_gabung_ganti_str.py
@@ -0,0 +1,27 @@
+# Fitur .split()
+# Memecah sebuah string berdasarkan string lainnya ke dalam sebuah list.
+print(">>> Fitur .split()")
+frasa = "ani dan budi dan wati dan johan"
+karakter = frasa.split("dan")
+print(karakter)
+kata = frasa.split(" ")
+print(kata)
+
+# Fitur .join()
+# Menggabungkan sebuah list yang berisikan string 
+#  berdasarkan sebuah string yang telah didefinisikan.
+print(">>> Fitur .join()")
+pemisah = " dan "
+karakter = ["Ricky", "Peter", "Jordan"]
+frasa = pemisah.join(karakter)
+print(frasa)
+frasa = " ".join(karakter)
+print(frasa)
+
+# Fitur .replace()
+# Menggantikan kemunculan suatu string 
+#  tertentu dengan string lainnya dalam sebuah string.
+print(">>> Fitur .replace()")
+frasa = "apel malang apel yang paling segar, apel sehat, apel nikmat"
+frasa = frasa.replace("apel", "jeruk")
+print(frasa)
