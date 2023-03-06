30a208069207aab33186ab48a968873618050070
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-06 07:50:28 +0700

    Create for_loops_1.py

---
+++ Python for Data Professional Beginner/Part 1/for_loops_1.py
@@ -0,0 +1,12 @@
+list_tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+
+# For loops with break
+print("For loops with break")
+total_tagihan_break = 0
+for tagihan in list_tagihan:
+    if tagihan < 0:
+        print("Terdapat angka minus dalam tagihan, perhitungan dihentikan!")
+        break
+    total_tagihan_break += tagihan
+print("Total tagihan %d." % total_tagihan_break)
+print()
