d356ff426427de79e2a638d00ed5ebc1990c42ab
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-06 10:15:14 +0700

    Update list_manipulation_1.py

--- Python for Data Professional Beginner/Part 2/list_manipulation_1.py
+++ Python for Data Professional Beginner/Part 2/list_manipulation_1.py
@@ -1,5 +1,6 @@
 # Fitur .index()
-# Mengembalikan indeks dari elemen pertama yang ditemukan dari awal sebuah list
+# Mengembalikan indeks dari elemen pertama yang ditemukan 
+#  dari awal sebuah list
 print(">>> Fitur .index()")
 list_score = ['Budi','Sud','Budi','Budi','Budi','Sud','Sud']
 score_pertama_sud = list_score.index('Sud') + 1
@@ -15,26 +16,27 @@ print(list_score)
 # Fitur .pop()
 # Menghilangkan elemen pada posisi tertentu
 print(">>> Fitur .pop()")
-list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_menu = ['Gado - gado', 'Ayam Goreng', 'Rendang']
 list_menu.pop(1)
 print(list_menu)
 
 # Fitur .remove()
 # Menghilangkan elemen dengan nilai tertentu
 print(">>> Fitur .remove()")
-list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu = ['Gado - gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
 list_menu.remove('Rendang')
 print(list_menu)
 
 # Fitur .reverse()
 # Membalik urutan elemen dari sebuah list
 print(">>> Fitur .reverse()")
-list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
+list_menu = ['Gado - gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
 list_menu.reverse()
 print(list_menu)
 
 # Fitur .sort()
-# Mengurutkan elemen pada sebuah list, secara default dengan urutan dari kecil ke besar (ascending)
+# Mengurutkan elemen pada sebuah list, secara default dengan 
+#  urutan dari kecil ke besar (ascending)
 print(">>> Fitur .sort()")
 list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang', 'Ketoprak']
 list_menu.sort() # Default: Ascending
