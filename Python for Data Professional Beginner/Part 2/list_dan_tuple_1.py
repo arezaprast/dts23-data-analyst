6b29bca6a24bf1aff1bacee6eacfdf4f4e0fee7b
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-06 07:59:00 +0700

    Create list_dan_tuple_1.py

---
+++ Python for Data Professional Beginner/Part 2/list_dan_tuple_1.py
@@ -0,0 +1,8 @@
+bulan_pembelian = ('januari', 'februari', 'maret', 'april', 'mei', 'juni', 'juli', 'agustus', 'september', 'oktober', 'november', 'desember')
+pertengahan_tahun = bulan_pembelian[4:8]
+print(pertengahan_tahun)
+awal_tahun = bulan_pembelian[:5]
+print(awal_tahun)
+akhir_tahun = bulan_pembelian[8:]
+print(akhir_tahun)
+print(bulan_pembelian[-4:-1])
