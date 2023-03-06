f1977f2d7a01efd933aeeb33cc5c94bf0c499247
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-06 09:50:20 +0700

    Create hilang_spasi.py

---
+++ Python for Data Professional Beginner/Part 2/hilang_spasi.py
@@ -0,0 +1,20 @@
+# Fitur .strip()
+# Menghilangkan kelebihan spasi pada awal dan akhir string
+print(">>> Fitur .strip()")
+kata_sambutan = ' halo, selamat siang! '
+kata_sambutan = kata_sambutan.strip()
+print(kata_sambutan)
+
+# Fitur .lstrip()
+# Menghilangkan kelebihan spasi pada awal string
+print(">>> Fitur .lstrip()")
+kata_sambutan = ' halo, selamat siang! '
+kata_sambutan = kata_sambutan.lstrip()
+print(kata_sambutan)
+
+# Fitur .rstrip()
+# Menghilangkan kelebihan spasi pada akhir string
+print(">>> Fitur .rstrip()")
+kata_sambutan = ' halo, selamat siang! '
+kata_sambutan = kata_sambutan.rstrip()
+print(kata_sambutan)
