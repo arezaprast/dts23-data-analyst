b9824289b60d01830b3e41c2ff6bdd185a13f95c
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-06 07:20:58 +0700

    Create tugas_praktek_barang.py

---
+++ Python for Data Professional Beginner/Part 1/tugas_praktek_barang.py
@@ -0,0 +1,3 @@
+sepatu = {"nama": "sepatu niko", "harga": 150000, "diskon": 30000 }
+baju = {"nama": "baju unikloh", "harga": 80000, "diskon": 8000}
+celana = {"nama": "celana lepis", "harga": 200000, "diskon": 60000}
