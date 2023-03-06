7fc2b1c50467b815762fc2d4edb700c242e24ecf
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-06 08:05:29 +0700

    Create list_manipulation.py

---
+++ Python for Data Professional Beginner/Part 2/list_manipulation.py
@@ -0,0 +1,41 @@
+# Fitur .append()
+# Menambahkan data sebagai elemen terakhir di list
+print(">>> Fitur .append()")
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_makanan.append('Ketoprak')
+print(list_makanan)
+
+# Fitur .clear()
+# Menghapus seluruh elemen dalam sebuah list
+print(">>> Fitur .clear()")
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_makanan.clear()
+print(list_makanan)
+
+# Fitur .copy()
+# Mengembalikan copy dari setiap elemen dalam list
+print(">>> Fitur .copy()")
+list_makanan1 = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_makanan2 = list_makanan1.copy()
+list_makanan3 = list_makanan1
+list_makanan2.append('Opor')
+list_makanan3.append('Ketoprak')
+print(list_makanan1)
+print(list_makanan2)
+
+# Fitur .count()
+# Mengembalikan jumlah kemunculan suatu elemen pada list
+print(">>> Fitur .count()")
+list_score = ['Budi', 'Sud', 'Budi', 'Budi', 'Budi', 'Sud', 'Sud']
+score_budi = list_score.count('Budi')
+score_sud = list_score.count('Sud')
+print(score_budi) # akan menampilkan output 4
+print(score_sud) # akan menampilkan output 3
+
+# Fitur .extend()
+# Menggabungkan dua buah list seperti penggunaan operator + untuk list
+print(">>> Fitur .extend()")
+list_menu = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_minuman = ['Es Teh', 'Es Jeruk', 'Es Campur']
+list_menu.extend(list_minuman)
+print(list_menu)
