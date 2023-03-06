c223dd0908ec75515d2f61ec6b0f8ef1c42f581d
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-06 09:47:16 +0700

    Create string_manipulation.py

---
+++ Python for Data Professional Beginner/Part 2/string_manipulation.py
@@ -0,0 +1,8 @@
+'''String Manipulation adalah teknik yang digunakan dalam memanipulasi data yang disimpan dalam tipe data str. 
+Pada bahasa Python, untuk mempermudah proses pengolahan data, tipe data string dapat diperlakukan layaknya seperti tipe data list.'''
+nama_produk = "Sepatu Niko"
+nama_produk = nama_produk[:2] + "P" + nama_produk[3:9] + "K" + nama_produk[-1]
+print(nama_produk)
+print(nama_produk[:7])
+print(nama_produk[7:])
+print(len(nama_produk))
