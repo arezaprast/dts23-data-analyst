1dcb2e38cde4101adeafa5f5592a8c9cd8110645
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-07 10:35:33 +0700

    Update 4.baca_teks_1.py

--- Python for Data Professional Beginner/Part 2/4.baca_teks_1.py
+++ Python for Data Professional Beginner/Part 2/4.baca_teks_1.py
@@ -7,3 +7,20 @@ print(response)
 print("\n>> Cetak isi file hello.txt menggunakan method response.iter_lines():")
 for baris in response.iter_lines():
 	print(baris)
+	
+# Membaca file hello.txt dengan fungsi read()
+''' print(" Membaca file hello.txt dengan fungsi read()")
+    file = open("Hello.txt", "r")
+    content = file.read()
+    file.close()
+    print(content)
+'''
+# Membaca file hello.txt dengan fungsi read line()
+''' print(" Membaca file hello.txt dengan fungsi readlines()")
+    file = open("Hello.txt", "r")
+    first_line = file.readline()
+    second_line = file.readline()
+    file.close()
+    print(first_line)
+    print(second_line)
+'''
