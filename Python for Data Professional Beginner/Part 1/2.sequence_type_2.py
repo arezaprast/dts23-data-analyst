8ee6e1b027e70d3ba7da794868507884ca1dbc18
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-07 08:47:28 +0700

    Update and rename sequence_type_part_2.py to 2.sequence_type_2.py

---
+++ Python for Data Professional Beginner/Part 1/2.sequence_type_2.py
@@ -0,0 +1,9 @@
+# Tipe data tuple juga berfungsi untuk menampung sekumpulan data. 
+#  Tipe data ini diawali dengan tanda kurung buka ( ( ), 
+#   memisahkan setiap elemen di dalamnya dengan tanda koma ( , ) dan 
+#    ditutup dengan tanda kurung tutup ( ) )
+
+contoh_tuple = ('januari', 'februari', 'maret', 'april')
+print(contoh_tuple[0])
+contoh_tuple = ('januari', 'februari', 'maret', 'april')
+contoh_tuple[0] = 'Desember'
