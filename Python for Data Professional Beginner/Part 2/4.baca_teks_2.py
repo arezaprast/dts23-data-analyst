5641b310895fa96468306d7679e5f034c556da87
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-07 10:35:53 +0700

    Update 4.baca_teks_2.py

--- Python for Data Professional Beginner/Part 2/4.baca_teks_2.py
+++ Python for Data Professional Beginner/Part 2/4.baca_teks_2.py
@@ -1,1 +1,23 @@
+import requests
+url = "https://storage.googleapis.com/dqlab-dataset/hello.txt"
+response = requests.get(url)
+# Cetak kode status dari response
+print(response)
+# Cetak isi file hello.txt menggunakan atribut response.text
+print("\n>> Cetak isi file hello.txt menggunakan atribut response.text:")
+print(response.text)
 
+# Membaca file hello.txt dengan fungsi redlines()
+''' print(">>> Membaca file hello.txt dengan fungsi readlines()")
+	file = open("hello.txt", "r")
+	all_lines = file.readlines()
+	file.close()
+	print(all_lines)
+'''
+# Membaca file hello.txt dengan menerapkan looping
+''' print(">>> Membaca file hello.txt dengan menerapkan looping")
+	file = open("hello.txt", "r")
+	for line in file:
+		print(line)
+	file.close()
+'''
