b2290f03f6ee2044cbea3ca24ee11c2694f17f5c
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-10 10:40:39 +0700

    Create 1.function_CEILING.sql

---
+++ Fundamental SQL/Using FUNCTION and GROUP BY/1.function_CEILING.sql
@@ -0,0 +1,3 @@
+select StudentID, FirstName, LastName, ceiling(Semester1) as Semester1, ceiling(Semester2) as Semester2, MarkGrowth from students;
+# Mengembalikan nilai integer terbesar yang terdekat dengan nilai input 
+# contoh : ceiling(17.34) = 17, ceiling(-95.34) = -95
