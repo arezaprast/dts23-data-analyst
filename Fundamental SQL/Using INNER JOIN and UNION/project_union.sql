ce9eb8e494aa0b50d5974fd5daa3142131daf04e
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-10 17:44:42 +0700

    Create project_union.sql

---
+++ Fundamental SQL/Using INNER JOIN and UNION/project_union.sql
@@ -0,0 +1,13 @@
+select 
+  nama_produk, kode_produk, harga
+from 
+  ms_produk_1
+where 
+  harga < 100000
+union
+select 
+  nama_produk, kode_produk, harga
+from 
+  ms_produk_2
+where 
+  harga < 50000;
