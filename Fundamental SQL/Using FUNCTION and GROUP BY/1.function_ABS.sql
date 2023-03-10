b7f9f2b4f2c6bcc2fa93e6b5ed3c9476e3ebe60c
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-10 10:42:13 +0700

    Update 1.function_ABS.sql

--- Fundamental SQL/Using FUNCTION and GROUP BY/1.function_ABS.sql
+++ Fundamental SQL/Using FUNCTION and GROUP BY/1.function_ABS.sql
@@ -1,1 +1,3 @@
 select StudentID, firstName, LastName, Semester1, Semester2, abs(MarkGrowth) as MarkGrowth from students;
+# Mengembalikan nilai absolute dari nilai input
+# contoh : abs(-17.6) = 17.6
