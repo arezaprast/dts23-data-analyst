1b2931d1e2dee20a88252a930cd285dc590b2fd9
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-10 17:41:34 +0700

    Create project_inner_join.sql

---
+++ Fundamental SQL/Using INNER JOIN and UNION/project_inner_join.sql
@@ -0,0 +1,10 @@
+select distinct 
+  ms_pelanggan.kode_pelanggan, ms_pelanggan.nama_customer, ms_pelanggan.alamat
+from 
+  ms_pelanggan
+inner join 
+  tr_penjualan
+on 
+  ms_pelanggan.kode_pelanggan = tr_penjualan.kode_pelanggan
+where 
+  tr_penjualan.nama_produk = 'Kotak Pensil DQlab' or tr_penjualan.nama_produk = 'Flashdisk DQlab 32 GB' or tr_penjualan.nama_produk = 'Sticky Notes DQlab 500 sheets';
