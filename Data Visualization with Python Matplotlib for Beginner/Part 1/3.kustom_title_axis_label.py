f60aca29bcb9c177bdd14894c2940cb44f7f03ff
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-20 10:00:04 +0700

    Update 3.kustom_title_axis_label.py

--- Data Visualization with Python Matplotlib for Beginner/Part 1/3.kustom_title_axis_label.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 1/3.kustom_title_axis_label.py
@@ -12,3 +12,13 @@ plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blu
 plt.xlabel('Order Month', fontsize=15)
 plt.ylabel('Total Amount', fontsize=15)
 plt.show()
+
+# Misalnya, untuk judul/title, parameter yang bisa ditambahkan:
+#   loc: digunakan untuk menentukan posisi title, misalnya ‘left’ untuk membuat rata kiri, ‘right’ untuk rata kanan, dan ‘center’ untuk meletakkannya di tengah.
+#         Jika tidak didefinisikan, maka default-nya title ada di tengah.
+#   pad: digunakan untuk menambahkan jarak antara judul ke grafik (dalam satuan px), misalnya kita tidak ingin judulnya terlalu menempel dengan grafiknya,
+#         jadi kita beri jarak.
+#   fontsize: digunakan untuk mengganti ukuran font/huruf (dalam satuan px).
+#   color: digunakan untuk mengganti warna huruf judul. Kita bisa menggunakan warna dasar dengan kata seperti ‘blue’, ‘red’, ‘orange’, dsb.
+#         Bisa juga dengan hex string, misalnya '#42DDF5' untuk warna biru muda.
+# Untuk xlabel dan ylabel, kita bisa mengganti fontsize dan color, tetapi tidak bisa mengganti loc.
