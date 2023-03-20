486347eb2e827e79eb7fa77b74e9df1da3583d36
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-20 10:01:30 +0700

    Update 3.kustom_line_point.py

--- Data Visualization with Python Matplotlib for Beginner/Part 1/3.kustom_line_point.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 1/3.kustom_line_point.py
@@ -12,3 +12,11 @@ plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blu
 plt.xlabel('Order Month', fontsize=15)
 plt.ylabel('Total Amount', fontsize=15)
 plt.show()
+
+# Untuk mengubah style dari garis maupun titik di chart, cukup dengan menambahkan parameter di function .plot(). Beberapa parameter yang bisa dikustomisasi:
+#   color: mengubah warnanya (sama seperti di title)
+#   linewidth: mengubah ketebalan line/garisnya (dalam satuan px)
+#   linestyle: mengubah jenis dari garis. Misalnya '-' atau 'solid' untuk garis tak terputus (seperti pada default), '--' atau 'dashed' untuk garis putus-putus,
+#              ':' atau 'dotted' untuk garis berupa titik-titik, bisa juga '-.' atau ‘dashdot’ untuk garis dan titik bergantian.
+#   marker: mengubah tipe points/titik data di chart. Ada banyak sekali kemungkinan nilai untuk marker ini, yang biasanya digunakan yaitu ‘.’ untuk bulatan kecil/titik,
+#           ‘o’ untuk bulatan agak besar, ‘s’ untuk persegi, ‘D’ untuk diamond/wajik, dan bentuk-bentuk lain seperti ‘+’, ‘x’, ‘|’, ‘*’.
