e75a57679777269bdc046fbff1a1a78c7854571a
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 16:39:46 +0700

    Add script

--- Python for Data Professional Beginner/Part 1/4.for_loops_3.py
+++ Python for Data Professional Beginner/Part 1/4.for_loops_3.py
@@ -2,4 +2,4 @@ list_daerah = ['Malang', 'Palembang', 'Medan']
 list_buah = ['Apel', 'Duku', 'Jeruk']
 for nama_daerah in list_daerah:
     for nama_buah in list_buah:
-        print(nama_buah+" "+nama_daerah)
+        print(nama_buah+" "+nama_daerah) 
