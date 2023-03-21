14493364cc9d80cd58feb18e76ce0754241d07c9
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 16:34:42 +0700

    Add script

--- Python for Data Professional Beginner/Part 1/2.sequence_type_2.py
+++ Python for Data Professional Beginner/Part 1/2.sequence_type_2.py
@@ -1,4 +1,4 @@
-contoh_tuple = ('januari', 'februari', 'maret', 'april')
+contoh_tuple = ('januari', 'februari', 'maret', 'april') 
 print(contoh_tuple[0])
 contoh_tuple = ('januari', 'februari', 'maret', 'april')
 contoh_tuple[0] = 'Desember'
