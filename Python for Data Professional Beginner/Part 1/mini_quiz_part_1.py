0730d1b3abb256c8c170fb0dea4bbccf0665fc32
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 16:24:26 +0700

    Add file

--- Python for Data Professional Beginner/Part 1/mini_quiz_part_1.py
+++ Python for Data Professional Beginner/Part 1/mini_quiz_part_1.py
@@ -7,7 +7,7 @@ list_plat_nomor = [8993, 2198, 2501, 2735, 3772, 4837, 9152]
 kendaraan_genap = 0 
 kendaraan_ganjil = 0
 for plat_nomor in list_plat_nomor:
-    if plat_nomor % 2 == 0:
+    if plat_nomor % 2==0:
         kendaraan_genap += 1 
     else:
         kendaraan_ganjil += 1
@@ -16,9 +16,9 @@ i = 1
 total_pengeluaran = 0
 while i <= jumlah_hari:
     if i % 2 == 0:
-        total_pengeluaran += (kendaraan_genap * uang_jalan)
+        total_pengeluaran += (kendaraan_genap*uang_jalan)
     else:
-        total_pengeluaran += (kendaraan_ganjil * uang_jalan) 
+        total_pengeluaran += (kendaraan_ganjil*uang_jalan) 
     i += 1
 # Cetak total pengeluaran
 print(total_pengeluaran)
