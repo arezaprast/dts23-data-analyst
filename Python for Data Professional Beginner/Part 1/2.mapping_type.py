3d70d6df631225bf078782ba99664e6579276ce6
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 16:20:37 +0700

    Update file

--- Python for Data Professional Beginner/Part 1/2.mapping_type.py
+++ Python for Data Professional Beginner/Part 1/2.mapping_type.py
@@ -4,10 +4,10 @@ print(person['pekerjaan'])
 
 # Tipe data mapping dapat digunakan untuk memetakan sebuah nilai 
 #  ke nilai lainnya. Dalam Python, tipe data mapping disebut 
-#   dengan istilah dictionary. Tipe data dictionary dapat 
-#    dideklarasikan dengan diawali oleh tanda kurung 
-#     buka kurawal ( { ), 
-# memisahkan setiap elemen di dalamnya dengan tanda koma ( , ) 
+#  dengan istilah dictionary. Tipe data dictionary dapat 
+#  dideklarasikan dengan diawali oleh tanda kurung 
+#  buka kurawal ( { ), 
+#  memisahkan setiap elemen di dalamnya dengan tanda koma ( , ) 
 #  dan ditutup dengan tanda kurung tutup ( } ). 
-#   Setiap elemen pada tipe data dictionary dideklarasikan 
-#    dengan format: "kunci" : "nilai"
+#  Setiap elemen pada tipe data dictionary dideklarasikan 
+#  dengan format: "kunci" : "nilai"
