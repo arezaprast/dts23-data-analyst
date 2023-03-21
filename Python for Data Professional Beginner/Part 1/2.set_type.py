9b5b6a23c559082c4ed84b7e69d1f3a92d535d76
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 16:25:13 +0700

    Update file

--- Python for Data Professional Beginner/Part 1/2.set_type.py
+++ Python for Data Professional Beginner/Part 1/2.set_type.py
@@ -7,14 +7,14 @@ print(contoh_frozen_set)
 
 # Tipe data set diawali dengan tanda kurung buka kurawal ( { ), 
 #  memisahkan setiap elemen di dalamnya dengan tanda koma ( , ) 
-#   dan ditutup dengan tanda kurung tutup ( } ). Namun berbeda 
-#    dengan tipe data sequence, seperti list, tipe data objek 
-#     tidak mengizinkan adanya elemen dengan nilai yang sama 
-#      dan tidak memperdulikan urutan dari elemen.
+#  dan ditutup dengan tanda kurung tutup ( } ). Namun berbeda 
+#  dengan tipe data sequence, seperti list, tipe data objek 
+#  tidak mengizinkan adanya elemen dengan nilai yang sama 
+#  dan tidak memperdulikan urutan dari elemen.
 # Tipe data frozenset sebenarnya hanya merupakan set yang 
 #  bersifat immutable, yang artinya setiap elemen di dalam 
-#   frozenset tidak dapat diubah setelah proses deklarasinya.
-#    Dari kedua contoh output pada program, dapat terlihat:
+#  frozenset tidak dapat diubah setelah proses deklarasinya.
+#  Dari kedua contoh output pada program, dapat terlihat:
 #    1. Berbeda dengan tipe data set, tipe data list memperdulikan 
 #       urutan dari setiap elemen saat list dideklarasikan.
 #    2. Berbeda dengan list yang mengizinkan adanya duplikasi 
