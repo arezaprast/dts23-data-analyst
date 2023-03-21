7127753a9d3a388ca30830407e8537f63a19c942
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 13:40:37 +0700

    Update by_year.sql

--- Project Data Analysis for Retail: Sales Performance Report/Store Promotion Effectiveness and Efficiency/by_year.sql
+++ Project Data Analysis for Retail: Sales Performance Report/Store Promotion Effectiveness and Efficiency/by_year.sql
@@ -1,1 +1,11 @@
-
+select 
+  year(order_date) as years, sum(sales) as sales, sum(discount_value) as promotion_value,
+  round((sum(discount_value)/sum(sales))*100,2) as burn_rate_percentage
+from 
+  dqlab_sales_store
+where 
+  order_status = 'Order Finished'
+group by 
+  year(order_date)
+order by 
+  years asc;
