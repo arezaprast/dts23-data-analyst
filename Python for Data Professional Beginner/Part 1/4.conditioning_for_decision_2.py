8b911512d96824fa95222d322134fc762bbb9b57
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 16:38:13 +0700

    Add script

--- Python for Data Professional Beginner/Part 1/4.conditioning_for_decision_2.py
+++ Python for Data Professional Beginner/Part 1/4.conditioning_for_decision_2.py
@@ -1,5 +1,5 @@
 jam = 13
-if jam >= 5 and jam < 12: # selama jam di antara 5 s.d. 12
+if jam >= 5 and jam < 12: # selama jam di antara 5 s.d. 12 
     print("Selamat pagi!")
 elif jam >= 12 and jam < 17: # selama jam di antara 12 s.d. 17
     print("Selamat siang!")
