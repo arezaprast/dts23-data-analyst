5d34f0e04d40a14d4deb3fae868a52e09d7eb67a
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 14:35:41 +0700

    Update hitung_presentasi.sql

--- Project Data Analysis for B2B Retail: Customer Analytics Report/hitung_presentasi.sql
+++ Project Data Analysis for B2B Retail: Customer Analytics Report/hitung_presentasi.sql
@@ -1,19 +1,11 @@
 select 
   quarter, sum(quantity) as total_penjualan, sum(quantity*priceeach) as revenue
 from 
-  (select 
-      orderNumber, status, quantity, priceeach, '1' as quarter
-   from 
-      orders_1 
-   where 
-      status = 'Shipped'
+  (select orderNumber, status, quantity, priceeach, '1' as quarter from orders_1 
+   where status = 'Shipped'
    union
-   select 
-      orderNumber, status, quantity, priceeach, '2' as quarter
-   from 
-      orders_2 
-   where 
-      status = 'Shipped'
+   select orderNumber, status, quantity, priceeach, '2' as quarter from orders_2 
+   where status = 'Shipped'
   ) as tabel_a
 group by 
   quarter;
