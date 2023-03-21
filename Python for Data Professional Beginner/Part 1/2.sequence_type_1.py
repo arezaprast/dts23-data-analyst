be1f4be291dd0f25087eab5ac0195cfaa947db8c
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 16:21:20 +0700

    Update file

--- Python for Data Professional Beginner/Part 1/2.sequence_type_1.py
+++ Python for Data Professional Beginner/Part 1/2.sequence_type_1.py
@@ -1,9 +1,10 @@
-# Tipe data list diawali dengan tanda kurung siku buka ( [ ), 
-#  memisahkan setiap elemen di dalamnya dengan tanda koma ( , ) 
-#   dan ditutup dengan kurung siku tutup ( ] )
 contoh_list = [1, 'dua', 3, 4.0, 5]
 print(contoh_list[0])
 print(contoh_list[3])
 contoh_list = [1, 'dua', 3, 4.0, 5]
 contoh_list[3] = 'empat' 
 print(contoh_list[3])
+
+# Tipe data list diawali dengan tanda kurung siku buka ( [ ), 
+#  memisahkan setiap elemen di dalamnya dengan tanda koma ( , ) 
+#  dan ditutup dengan kurung siku tutup ( ] )
