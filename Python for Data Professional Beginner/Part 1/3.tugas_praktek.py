ea55a15794395fb8d54ca031fd3588e4bbb5dfbc
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 16:36:42 +0700

    Add script

--- Python for Data Professional Beginner/Part 1/3.tugas_praktek.py
+++ Python for Data Professional Beginner/Part 1/3.tugas_praktek.py
@@ -1,8 +1,8 @@
-sepatu = { "nama" : "Sepatu Niko", "harga": 150000, "diskon": 30000 }
-baju = { "nama" : "Baju Unikloh", "harga": 80000, "diskon": 8000 }
-celana = { "nama" : "Celana Lepis", "harga": 200000, "diskon": 60000 }
-harga_sepatu = sepatu["harga"] - sepatu["diskon"]
-harga_baju = baju["harga"] - baju["diskon"]
-harga_celana = celana["harga"] - celana["diskon"]
-total_harga = (harga_baju + harga_celana + harga_sepatu) * 1.1 
+sepatu = {"nama":"Sepatu Niko", "harga":150000, "diskon":30000}
+baju = {"nama":"Baju Unikloh", "harga":80000, "diskon":8000}
+celana = {"nama":"Celana Lepis", "harga":200000, "diskon":60000}
+harga_sepatu = sepatu["harga"]-sepatu["diskon"]
+harga_baju = baju["harga"]-baju["diskon"]
+harga_celana = celana["harga"]-celana["diskon"]
+total_harga = (harga_baju+harga_celana+harga_sepatu)*1.1 
 print(total_harga)
