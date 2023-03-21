e35188e5b1c8d7c1122dfd1b46ecd9230dcf856b
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 16:32:35 +0700

    Add script

--- Python for Data Professional Beginner/Part 1/2.tugas_praktek_2.py
+++ Python for Data Professional Beginner/Part 1/2.tugas_praktek_2.py
@@ -1,4 +1,4 @@
 sepatu = {"nama":"Sepatu Niko", "harga":150000, "diskon":30000} 
 baju = {"nama":"Baju Unikloh", "harga":80000, "diskon":8000} 
 celana = {"nama":"Celana Lepis", "harga":200000, "diskon":60000} 
-daftar_belanja = [sepatu, baju, celana]
+daftar_belanja = [sepatu,baju,celana]
