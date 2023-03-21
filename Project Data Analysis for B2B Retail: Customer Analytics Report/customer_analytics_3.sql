cf5013a6dc2cd72743d263fc287e916251b311a5
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 16:04:04 +0700

    Create customer_analytics_3.sql

---
+++ Project Data Analysis for B2B Retail: Customer Analytics Report/customer_analytics_3.sql
@@ -0,0 +1,12 @@
+select
+  left(productCode, 3) as categoryid,
+  count(distinct orderNumber) as total_order,
+  sum(quantity) as total_penjualan
+from
+  (select productCode, orderNumber, quantity, status from orders_2) as tabel_c
+where
+  status = 'Shipped'
+group by
+  categoryid
+order by
+  total_order desc;
