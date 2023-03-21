bed759f8114cd4b91f243e5f0fd8aeec98855805
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-21 14:26:53 +0700

    Create total_penjualan_dan_revenue.sql

---
+++ Project Data Analysis for B2B Retail: Customer Analytics Report/total_penjualan_dan_revenue.sql
@@ -0,0 +1,6 @@
+select sum(quantity) as total_penjualan, sum(quantity*priceeach) as revenue
+from orders_1
+where status = 'Shipped';
+select sum(quantity) as total_penjualan, sum(quantity*priceeach) as revenue
+from orders_2
+where status = 'Shipped';
