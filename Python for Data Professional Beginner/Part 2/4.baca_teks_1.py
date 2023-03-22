90dfd7f07fc49e7e436f7dbcaeb1254aa98c7093
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 18:57:26 +0700

    Update 4.baca_teks_1.py

--- Python for Data Professional Beginner/Part 2/4.baca_teks_1.py
+++ Python for Data Professional Beginner/Part 2/4.baca_teks_1.py
@@ -1,7 +1,7 @@
 import requests
 url = "https://storage.googleapis.com/dqlab-dataset/hello.txt"
 response = requests.get(url)
-# Cetak kode status dari response
+# Cetak kode status dari response 
 print(response)
 # Cetak isi file hello.txt menggunakan method response.iter_lines()
 print("\n>> Cetak isi file hello.txt menggunakan method response.iter_lines():")
