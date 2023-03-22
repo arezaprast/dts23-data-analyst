c5fcbedf63716adbde8f944a52840cb1d7bc5f07
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 21:15:10 +0700

    Add script

--- Fundamental SQL/Using FUNCTION and GROUP BY/1.tugas_praktek.sql
+++ Fundamental SQL/Using FUNCTION and GROUP BY/1.tugas_praktek.sql
@@ -1,1 +1,1 @@
-select StudentID, FirstName, LastName, mod(Semester1, 2) as Semester1, Semester2, exp(MarkGrowth) from students;
+select StudentID, FirstName, LastName, mod(Semester1,2) as Semester1, Semester2, exp(MarkGrowth) from students;
