70008dff063bd6d1f34f5cc4c8aafd6d1abef23c
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 18:54:01 +0700

    Add script

--- Python for Data Professional Beginner/Part 2/2.string_manipulation.py
+++ Python for Data Professional Beginner/Part 2/2.string_manipulation.py
@@ -1,7 +1,7 @@
 # String Manipulation adalah teknik yang digunakan dalam memanipulasi data
 #  yang disimpan dalam tipe data str. Pada bahasa Python, untuk mempermudah 
-#   proses pengolahan data, tipe data string dapat diperlakukan layaknya 
-#    seperti tipe data list.
+#  proses pengolahan data, tipe data string dapat diperlakukan layaknya 
+#  seperti tipe data list.
 nama_produk = "Sepatu Niko"
 nama_produk = nama_produk[:2] + "P" + nama_produk[3:9] + "K" + nama_produk[-1]
 print(nama_produk)
