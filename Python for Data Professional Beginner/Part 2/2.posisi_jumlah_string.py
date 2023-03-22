95e2132baca0abb7e7f16fb6575d5033c2bf3a90
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 18:53:11 +0700

    Add script

--- Python for Data Professional Beginner/Part 2/2.posisi_jumlah_string.py
+++ Python for Data Professional Beginner/Part 2/2.posisi_jumlah_string.py
@@ -1,14 +1,13 @@
 teks = "Apel malang adalah apel termanis dibanding apel - apel lainnya"
 # Fitur .find()
-# Mengembalikan posisi dari sebuah teks (sub - string) 
-#  lainnya dalam sebuah string
+#   Mengembalikan posisi dari sebuah teks (sub - string) 
+#   lainnya dalam sebuah string
 print(">>> Fitur .find()")
 print(teks.find("Apel"))
 print(teks.find("malang"))
-
 # Fitur .count()
-# Menghitung jumlah kemunculan sebuah teks (string) lainnya 
-#  dalam suatu string (string yang dicari bersifat case sensitive).
+#   Menghitung jumlah kemunculan sebuah teks (string) lainnya 
+#   dalam suatu string (string yang dicari bersifat case sensitive).
 print(">>> Fitur .count()")
 kemunculan_kata_apel = teks.count("apel")
 print(kemunculan_kata_apel)
