eba84640242e286bcdb9baf465d340a36655280c
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 18:50:57 +0700

    Add script

--- Python for Data Professional Beginner/Part 2/1.tuple_manipulation.py
+++ Python for Data Professional Beginner/Part 2/1.tuple_manipulation.py
@@ -1,15 +1,14 @@
 # Fitur .count()
-# Mengembalikan jumlah kemunculan suatu elemen pada tuple
+#   Mengembalikan jumlah kemunculan suatu elemen pada tuple
 print(">>> Fitur .count()")
 tuple_score = ('Budi', 'Sud', 'Budi', 'Budi', 'Budi', 'Sud', 'Sud')
 score_budi = tuple_score.count('Budi')
 score_sud = tuple_score.count('Sud')
 print(score_budi) # akan menampilkan output 4
 print(score_sud) # akan menampilkan output 3
-
 # Fitur .index()
-# Mengembalikan indeks dari elemen pertama yang ditemukan 
-#  dari awal sebuah tuple
+#   Mengembalikan indeks dari elemen pertama yang ditemukan 
+#   dari awal sebuah tuple
 print(">>> Fitur .index()")
 tuple_score = ('Budi','Sud','Budi','Budi','Budi','Sud','Sud')
 score_pertama_sud = tuple_score.index('Sud') + 1
