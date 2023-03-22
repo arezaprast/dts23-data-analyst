bb821e2affd26c1c95dc0580ee5b299abd360b71
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 21:22:05 +0700

    Add script

--- Fundamental SQL/Using FUNCTION and GROUP BY/mini_project_1.sql
+++ Fundamental SQL/Using FUNCTION and GROUP BY/mini_project_1.sql
@@ -1,10 +1,17 @@
 ## 1. Total jumlah seluruh penjualan (total/revenue).
-SELECT sum(total) as total 
-FROM tr_penjualan;
+SELECT 
+  sum(total) as total 
+FROM 
+  tr_penjualan;
 ## 2. Total quantity seluruh produk yang terjual.
-SELECT sum(qty) as qty 
-FROM tr_penjualan;
+SELECT 
+  sum(qty) as qty 
+FROM 
+  tr_penjualan;
 ## 3. Total quantity dan total revenue untuk setiap kode produk.
-SELECT kode_produk, sum(qty) as qty, sum(total) as total 
-FROM tr_penjualan
-GROUP BY kode_produk;
+SELECT 
+  kode_produk, sum(qty) as qty, sum(total) as total 
+FROM 
+  tr_penjualan
+GROUP BY 
+  kode_produk;
