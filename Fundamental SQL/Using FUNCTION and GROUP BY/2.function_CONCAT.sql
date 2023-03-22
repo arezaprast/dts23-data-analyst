966214b47c6fbed885dbbddef66df1745d3a9d2e
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 21:15:30 +0700

    Add script

--- Fundamental SQL/Using FUNCTION and GROUP BY/2.function_CONCAT.sql
+++ Fundamental SQL/Using FUNCTION and GROUP BY/2.function_CONCAT.sql
@@ -1,2 +1,2 @@
 select StudentID, concat(FirstName, LastName) as Name, Semester1, Semester2, MarkGrowth from students;
-# Mengabungkan semua argumen/input, NULL value akan diabaikan
+# Mengabungkan semua argumen/input, NULL value akan diabaikan 
