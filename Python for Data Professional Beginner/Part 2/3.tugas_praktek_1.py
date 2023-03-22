39d1ad910ce21b489f96785a7fc81f5947e3c118
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 18:56:35 +0700

    Add script

--- Python for Data Professional Beginner/Part 2/3.tugas_praktek_1.py
+++ Python for Data Professional Beginner/Part 2/3.tugas_praktek_1.py
@@ -1,4 +1,4 @@
-# Dua buah data yang tersimpan dalam tipe list
+# Dua buah data yang tersimpan dalam tipe list 
 data1 = [70, 70, 70, 100, 100, 100, 120, 120, 150, 150]
 data2 = [50, 60, 60, 50, 70, 70, 100, 80, 100, 90]
 # Definisikan fungsi hitng_rata_rata
