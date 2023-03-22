00d246c247466f5052142016b079f959e3034f13
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 19:00:38 +0700

    Add script

--- Python for Data Professional Beginner/Part 2/4.tulis_teks_2.py
+++ Python for Data Professional Beginner/Part 2/4.tulis_teks_2.py
@@ -1,4 +1,4 @@
-# Menulis ke file dengan mode append
+# Menulis ke file dengan mode append 
 file = open("hello.txt", "a")
 file.writelines([
 "Sekarang kita belajar menulis dengan menggunakan Python", 
