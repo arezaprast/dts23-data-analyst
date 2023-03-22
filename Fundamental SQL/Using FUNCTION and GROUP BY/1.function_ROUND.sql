c6a6bb873f23a40dc1552f52d99cbd9425285ab1
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 21:14:29 +0700

    Add script

--- Fundamental SQL/Using FUNCTION and GROUP BY/1.function_ROUND.sql
+++ Fundamental SQL/Using FUNCTION and GROUP BY/1.function_ROUND.sql
@@ -1,2 +1,2 @@
 select StudentID, FirstName, LastName, round(Semester1, 1) as Semester1, round(Semester2, 0) as Semester2, MarkGrowth from students;
-# Mengembalikan nilai pembulatan dari suatu nilai desimal
+# Mengembalikan nilai pembulatan dari suatu nilai desimal 
