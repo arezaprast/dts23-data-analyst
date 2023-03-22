173b66c32ae21cc6f18654b57008f90e835b778e
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 18:53:41 +0700

    Add script

--- Python for Data Professional Beginner/Part 2/2.startswith_endswith_string.py
+++ Python for Data Professional Beginner/Part 2/2.startswith_endswith_string.py
@@ -1,14 +1,13 @@
 # Fitur .startswith()
-# Mengembalikan nilai kebenaran True ketika sebuah teks (string) 
-#  diawali dengan sebuah teks lainnya.
+#   Mengembalikan nilai kebenaran True ketika sebuah teks (string) 
+#   diawali dengan sebuah teks lainnya.
 print(">>> Fitur .startswith()")
 teks = "Apel malang adalah apel termanis dibanding apel - apel lainnya"
 print(teks.startswith("Apel"))
 print(teks.startswith("apel"))
-
 # Fitur .endswith()
-# Mengembalikan nilai kebenaran True ketika sebuah teks (string) 
-#  diakhiri dengan sebuah teks lainnya.
+#   Mengembalikan nilai kebenaran True ketika sebuah teks (string) 
+#   diakhiri dengan sebuah teks lainnya.
 print(">>> Fitur .endswith()")
 print(teks.endswith("lainnya"))
 print(teks.endswith("apel"))
