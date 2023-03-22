86d6dabc274abda2f7cf3c8133c561787e498dda
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 18:55:12 +0700

    Add script

--- Python for Data Professional Beginner/Part 2/2.ubah_caps_string.py
+++ Python for Data Professional Beginner/Part 2/2.ubah_caps_string.py
@@ -1,17 +1,15 @@
 # Fitur .capitalize()
-# Mengubah elemen pertama dari string menjadi huruf kapital.
+#   Mengubah elemen pertama dari string menjadi huruf kapital.
 print(">>> Fitur .capitalize()")
 judul_buku = 'belajar bahasa Python'
 print(judul_buku.capitalize())
-
 # Fitur .lower()
-# Mengubah seluruh huruf dalam teks (string) menjadi huruf kecil.
+#   Mengubah seluruh huruf dalam teks (string) menjadi huruf kecil.
 print(">>> Fitur .lower()")
 judul_buku = 'Belajar Bahasa PYTHON.'
 print(judul_buku.lower())
-
 # Fitur .upper()
-# Mengubah seluruh huruf dalam teks (string) menjadi huruf kecil.
+#   Mengubah seluruh huruf dalam teks (string) menjadi huruf kecil.
 print(">>> Fitur .upper()")
 judul_buku = 'Belajar Bahasa PYTHON.'
 print(judul_buku.upper())
