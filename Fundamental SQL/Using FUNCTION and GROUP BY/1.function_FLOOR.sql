ae21ce54bf095b19f4f1a3910b66b63b017a1334
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 21:14:00 +0700

    Add script

--- Fundamental SQL/Using FUNCTION and GROUP BY/1.function_FLOOR.sql
+++ Fundamental SQL/Using FUNCTION and GROUP BY/1.function_FLOOR.sql
@@ -1,2 +1,2 @@
 select StudentID, FirstName, LastName, ceiling(Semester1) as Semester1, ceiling(Semester2) as Semester2, MarkGrowth from students;
-# Mengembalikan nilai integer terkecil yang terdekat dengan nilai input
+# Mengembalikan nilai integer terkecil yang terdekat dengan nilai input 
