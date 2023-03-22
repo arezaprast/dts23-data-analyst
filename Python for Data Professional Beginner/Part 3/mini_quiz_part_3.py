fb8e9cc646bcc22dffd2f10f72a171e87e521284
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-22 19:06:02 +0700

    Add script

--- Python for Data Professional Beginner/Part 3/mini_quiz_part_3.py
+++ Python for Data Professional Beginner/Part 3/mini_quiz_part_3.py
@@ -12,36 +12,30 @@ class Karyawan:
         self.pendapatan_tambahan += jumlah_tambahan
     def total_pendapatan(self):
         return self.pendapatan + self.pendapatan_tambahan
-
 # Definisikan class TenagaLepas sebagai child class dari class Karyawan 
 class TenagaLepas(Karyawan):
     def __init__(self, nama, usia, pendapatan): 
         super().__init__(nama, usia, pendapatan, 0)
     def tambahan_proyek(self, nilai_proyek): 
         self.pendapatan_tambahan += nilai_proyek * 0.01
-
 # Definisikan class AnalisData sebagai child class dari class Karyawan 
 class AnalisData(Karyawan):
     def __init__(self, nama, usia = 21, pendapatan = 6500000, insentif_lembur = 100000):
         super().__init__(nama, usia, pendapatan, insentif_lembur) 
-
 # Definisikan class IlmuwanData sebagai child class dari class Karyawan
 class IlmuwanData(Karyawan):
     def __init__(self, nama, usia = 25, pendapatan = 12000000, insentif_lembur = 150000):
         super().__init__(nama, usia, pendapatan, insentif_lembur) 
     def tambahan_proyek(self, nilai_proyek):
         self.pendapatan_tambahan += 0.1 * nilai_proyek 
-
 # Definisikan class PembersihData sebagai child class dari class TenagaLepas
 class PembersihData(TenagaLepas):
     def __init__(self, nama, usia, pendapatan = 4000000): 
         super().__init__(nama, usia, pendapatan)
-
 # Definisikan class DokumenterTeknis sebagai child class dari class TenagaLepas
 class DokumenterTeknis(TenagaLepas):
     def __init__(self, nama, usia, pendapatan = 2500000): 
         super().__init__(nama, usia, pendapatan)
-
 # Definisikan class Perusahaan 
 class Perusahaan:
     def __init__(self, nama, alamat, nomor_telepon): 
@@ -69,7 +63,6 @@ class Perusahaan:
             if karyawan.nama == nama_karyawan: 
                 return karyawan
         return None
-
 # Create object karyawan sesuai dengan tugasnya masing-masing
 # seperti yang dinyatakan dalam tabel.
 ani = PembersihData('Ani', 25)
@@ -78,10 +71,8 @@ cici = IlmuwanData('Cici')
 didi = IlmuwanData('Didi', 32, 20000000)
 efi = AnalisData('Efi')
 febi = AnalisData('Febi', 28, 12000000)
-
 # Create object perusahaan
 perusahaan = Perusahaan('ABC', 'Jl. Jendral Sudirman, Blok 11', '(021) 95812XX')
-
 # Aktifkan setiap karyawan yang telah didefinisikan
 perusahaan.aktifkan_karyawan(ani)
 perusahaan.aktifkan_karyawan(budi)
@@ -89,6 +80,5 @@ perusahaan.aktifkan_karyawan(cici)
 perusahaan.aktifkan_karyawan(didi)
 perusahaan.aktifkan_karyawan(efi)
 perusahaan.aktifkan_karyawan(febi)
-
 # Cetak keseluruhan total pengeluaran perusahaan
 print(perusahaan.total_pengeluaran())
