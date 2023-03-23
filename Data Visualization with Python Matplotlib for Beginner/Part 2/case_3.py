1e9b02b12ba6a868917d77a0b254f5174af3d881
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 13:04:23 +0700

    Add script

--- Data Visualization with Python Matplotlib for Beginner/Part 2/case_3.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/case_3.py
@@ -9,7 +9,7 @@ top_brands = (dataset[dataset['order_month']=='2019-12'].groupby('brand')['quant
 dataset_top5brand_dec = dataset[(dataset['order_month']=='2019-12') & (dataset['brand'].isin(top_brands['brand'].to_list()))]
 plt.clf()
 dataset_top5brand_dec.groupby('brand')['product_id'].nunique().sort_values(ascending=False).plot(kind='bar', color='green')
-plt.title('Number of Sold Products per Brand, December 2019',loc='center',pad=30, fontsize=15, color='blue')
+plt.title('Number of Sold Products per Brand, December 2019',loc='center',pad=30,fontsize=15,color='blue')
 plt.xlabel('Brand', fontsize = 15)
 plt.ylabel('Number of Products',fontsize = 15)
 plt.ylim(ymin=0)
