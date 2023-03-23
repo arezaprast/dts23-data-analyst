ede5ac8f8a19132ea10ef3c784002ed88430ca2d
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 21:08:50 +0700

    Add script

--- Project Data Analysis for B2B Retail: Customer Analytics Report/customer_analytics_3.sql
+++ Project Data Analysis for B2B Retail: Customer Analytics Report/customer_analytics_3.sql
@@ -3,7 +3,10 @@ select
   count(distinct orderNumber) as total_order,
   sum(quantity) as total_penjualan
 from
-  (select productCode, orderNumber, quantity, status from orders_2) as tabel_c
+  (select 
+    productCode, orderNumber, quantity, status 
+   from 
+    orders_2) as tabel_c
 where
   status = 'Shipped'
 group by
