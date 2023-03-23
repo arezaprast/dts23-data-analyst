ebd910922eede4e4cc242576fb734e7fbd92f6d9
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 10:23:47 +0700

    Add script

--- Exploratory Data Analysis with Python for Beginner/3.tugas_praktek_2.py
+++ Exploratory Data Analysis with Python for Beginner/3.tugas_praktek_2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 order_df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/order.csv")
-# Standar variasi kolom product_weight_gram
+# Standar variasi kolom product_weight_gram 
 order_df.loc[:,"product_weight_gram"].std()
 # Varians kolom product_weight_gram
 order_df.loc[:,"product_weight_gram"].var()
