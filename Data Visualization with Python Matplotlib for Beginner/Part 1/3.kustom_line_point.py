d28458b2851724515a7d8a68ab6dbe22ba67c9eb
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 12:51:55 +0700

    Add script

--- Data Visualization with Python Matplotlib for Beginner/Part 1/3.kustom_line_point.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 1/3.kustom_line_point.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
-
 dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
 dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
 dataset['gmv'] = dataset['item_price']*dataset['quantity']
-
 plt.figure(figsize=(15, 5))
 dataset.groupby(['order_month'])['gmv'].sum().plot(color='green', marker='o', linestyle='-.', linewidth=2)
 plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
@@ -16,7 +14,9 @@ plt.show()
 # Untuk mengubah style dari garis maupun titik di chart, cukup dengan menambahkan parameter di function .plot(). Beberapa parameter yang bisa dikustomisasi:
 #   color: mengubah warnanya (sama seperti di title)
 #   linewidth: mengubah ketebalan line/garisnya (dalam satuan px)
-#   linestyle: mengubah jenis dari garis. Misalnya '-' atau 'solid' untuk garis tak terputus (seperti pada default), '--' atau 'dashed' untuk garis putus-putus,
-#              ':' atau 'dotted' untuk garis berupa titik-titik, bisa juga '-.' atau ‘dashdot’ untuk garis dan titik bergantian.
-#   marker: mengubah tipe points/titik data di chart. Ada banyak sekali kemungkinan nilai untuk marker ini, yang biasanya digunakan yaitu ‘.’ untuk bulatan kecil/titik,
-#           ‘o’ untuk bulatan agak besar, ‘s’ untuk persegi, ‘D’ untuk diamond/wajik, dan bentuk-bentuk lain seperti ‘+’, ‘x’, ‘|’, ‘*’.
+#   linestyle: mengubah jenis dari garis. Misalnya '-' atau 'solid' untuk garis tak terputus (seperti pada default),
+#             '--' atau 'dashed' untuk garis putus-putus,
+#             ':' atau 'dotted' untuk garis berupa titik-titik, bisa juga '-.' atau ‘dashdot’ untuk garis dan titik bergantian.
+#   marker: mengubah tipe points/titik data di chart. Ada banyak sekali kemungkinan nilai untuk marker ini, yang biasanya digunakan 
+#           yaitu ‘.’ untuk bulatan kecil/titik, ‘o’ untuk bulatan agak besar, ‘s’ untuk persegi, ‘D’ untuk diamond/wajik,
+#           dan bentuk-bentuk lain seperti ‘+’, ‘x’, ‘|’, ‘*’.
