407d6c285490164861b09118377e786d7a3a7021
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 12:58:56 +0700

    Add script

--- Data Visualization with Python Matplotlib for Beginner/Part 2/3.bar_chart.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/3.bar_chart.py
@@ -5,7 +5,6 @@ dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_r
 dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
 dataset['gmv'] = dataset['item_price']*dataset['quantity']
 dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
-
 plt.clf()
 dataset_dki_q4.groupby('city')['gmv'].sum().sort_values(ascending=False).plot(kind='bar', color='green')
 plt.title('GMV Per City - DKI Jakarta in Q4 2019', loc='center', pad=30, fontsize=15, color='blue')
