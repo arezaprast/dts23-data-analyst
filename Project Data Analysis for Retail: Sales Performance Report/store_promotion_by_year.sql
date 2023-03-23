26b61476980cdd768d8da123a591dc8123cd597e
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 20:50:07 +0700

    Add script

--- Project Data Analysis for Retail: Sales Performance Report/store_promotion_by_year.sql
+++ Project Data Analysis for Retail: Sales Performance Report/store_promotion_by_year.sql
@@ -1,5 +1,7 @@
 select 
-  year(order_date) as years, sum(sales) as sales, sum(discount_value) as promotion_value,
+  year(order_date) as years, 
+  sum(sales) as sales, 
+  sum(discount_value) as promotion_value,
   round((sum(discount_value)/sum(sales))*100,2) as burn_rate_percentage
 from 
   dqlab_sales_store
