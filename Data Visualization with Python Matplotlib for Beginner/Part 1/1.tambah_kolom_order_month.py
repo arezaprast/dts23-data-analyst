426119fc63598ba05dfd5ac8f302ef869b873e80
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 12:49:27 +0700

    Add script

--- Data Visualization with Python Matplotlib for Beginner/Part 1/1.tambah_kolom_order_month.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 1/1.tambah_kolom_order_month.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import datetime
-
 dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
-
 dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d').strftime('%Y-%m'))
 print(dataset.head())
 
 # Ada beberapa function yang berperan di sini:
 #   apply & lambda biasa digunakan untuk membuat kolom baru, berdasarkan suatu kolom lain yang sudah ada
-#     (misal .apply(lambda x: x*2) berarti setiap input x di dalam kolom, akan diubah menjadi x*2). 
-#      Dalam hal ini kolom yang sudah ada adalah dataset['order_date'], lalu tiap nilai di dalamnya kita proses agar menjadi month-nya saja
+#   (misal .apply(lambda x: x*2) berarti setiap input x di dalam kolom, akan diubah menjadi x*2). 
+# Dalam hal ini kolom yang sudah ada adalah dataset['order_date'], lalu tiap nilai di dalamnya kita proses agar menjadi month-nya saja
 #   Function datetime.datetime.strptime digunakan untuk mengubah date/time dalam bentuk string menjadi tipe data datetime.
 #   Function strftime digunakan untuk mengubah format suatu data bertipe datetime, dalam hal ini diubah menjadi '%Y-%m', 
-#     yang berarti output-nya adalah waktu dengan bentuk YYYY-MM atau tahun dan bulan saja, tanggalnya sudah tidak ada. 
+#   yang berarti output-nya adalah waktu dengan bentuk YYYY-MM atau tahun dan bulan saja, tanggalnya sudah tidak ada.
