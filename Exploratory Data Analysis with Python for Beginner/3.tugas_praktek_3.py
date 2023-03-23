5b0338e6f2063799ebc2186c06d50aaa671e07b6
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 10:24:06 +0700

    Add script

--- Exploratory Data Analysis with Python for Beginner/3.tugas_praktek_3.py
+++ Exploratory Data Analysis with Python for Beginner/3.tugas_praktek_3.py
@@ -4,6 +4,6 @@ order_df = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/order.csv")
 Q1 = order_df[["product_weight_gram"]].quantile(0.25)
 # Hitung quartile 3
 Q3 = order_df[["product_weight_gram"]].quantile(0.75)
-# Hitung inter quartile range dan cetak ke console
+# Hitung inter quartile range dan cetak ke console 
 IQR = Q3 - Q1
 print(IQR)
