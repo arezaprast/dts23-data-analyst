52d0bde265c5cdfb381ed80379b28497530212df
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 21:05:22 +0700

    Add script

--- Project Data Analysis for B2B Retail: Customer Analytics Report/total_penjualan_dan_revenue.sql
+++ Project Data Analysis for B2B Retail: Customer Analytics Report/total_penjualan_dan_revenue.sql
@@ -1,6 +1,14 @@
-select sum(quantity) as total_penjualan, sum(quantity*priceeach) as revenue
-from orders_1
-where status = 'Shipped';
-select sum(quantity) as total_penjualan, sum(quantity*priceeach) as revenue
-from orders_2
-where status = 'Shipped';
+select 
+  sum(quantity) as total_penjualan, 
+  sum(quantity*priceeach) as revenue
+from 
+  orders_1
+where 
+  status = 'Shipped';
+select 
+  sum(quantity) as total_penjualan, 
+  sum(quantity*priceeach) as revenue
+from 
+  orders_2
+where 
+  status = 'Shipped';
