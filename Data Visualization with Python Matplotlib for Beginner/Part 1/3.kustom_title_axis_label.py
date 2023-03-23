07bf54e0af4e66a59ee00e95fd829b604a855fef
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 12:53:03 +0700

    Add script

--- Data Visualization with Python Matplotlib for Beginner/Part 1/3.kustom_title_axis_label.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 1/3.kustom_title_axis_label.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
-
 dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
 dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
 dataset['gmv'] = dataset['item_price']*dataset['quantity']
-
 plt.figure(figsize=(15, 5))
 dataset.groupby(['order_month'])['gmv'].sum().plot()
 plt.title('Monthly GMV Year 2019', loc='center', pad=20, fontsize=20, color='blue')
@@ -14,10 +12,11 @@ plt.ylabel('Total Amount', fontsize=15)
 plt.show()
 
 # Misalnya, untuk judul/title, parameter yang bisa ditambahkan:
-#   loc: digunakan untuk menentukan posisi title, misalnya ‘left’ untuk membuat rata kiri, ‘right’ untuk rata kanan, dan ‘center’ untuk meletakkannya di tengah.
-#         Jika tidak didefinisikan, maka default-nya title ada di tengah.
-#   pad: digunakan untuk menambahkan jarak antara judul ke grafik (dalam satuan px), misalnya kita tidak ingin judulnya terlalu menempel dengan grafiknya,
-#         jadi kita beri jarak.
+#   loc: digunakan untuk menentukan posisi title, misalnya ‘left’ untuk membuat rata kiri, ‘right’ untuk rata kanan,
+#        dan ‘center’ untuk meletakkannya di tengah.
+#        Jika tidak didefinisikan, maka default-nya title ada di tengah.
+#   pad: digunakan untuk menambahkan jarak antara judul ke grafik (dalam satuan px), misalnya kita tidak ingin judulnya
+#        terlalu menempel dengan grafiknya, jadi kita beri jarak.
 #   fontsize: digunakan untuk mengganti ukuran font/huruf (dalam satuan px).
 #   color: digunakan untuk mengganti warna huruf judul. Kita bisa menggunakan warna dasar dengan kata seperti ‘blue’, ‘red’, ‘orange’, dsb.
 #         Bisa juga dengan hex string, misalnya '#42DDF5' untuk warna biru muda.
