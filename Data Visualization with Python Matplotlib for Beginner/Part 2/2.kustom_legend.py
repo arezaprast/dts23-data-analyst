692da59decb12f1de648b88eeb1cf6291c7b2e90
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 12:57:18 +0700

    Add script

--- Data Visualization with Python Matplotlib for Beginner/Part 2/2.kustom_legend.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/2.kustom_legend.py
@@ -18,17 +18,18 @@ plt.tight_layout()
 plt.show()
 
 # Beberapa parameter yang bisa ditambahkan untuk legend:
-
-# loc: untuk menentukan posisi legend, berikut beberapa lokasi legend yang bisa didefinisikan:
-#     'upper left', 'upper right', 'lower left', 'lower right':legend diletakkan di pojok dari axes (atas kiri, atas kanan, bawah kiri, atas kiri)
-#     'upper center', 'lower center', 'center left', 'center right': legend diletakkan di tepi axes (atas tengah, bawah tengah, tengah kiri, tengah kanan)
+#   loc: untuk menentukan posisi legend, berikut beberapa lokasi legend yang bisa didefinisikan:
+#     'upper left', 'upper right', 'lower left', 'lower right':legend diletakkan di pojok dari axes 
+#       (atas kiri, atas kanan, bawah kiri, atas kiri)
+#     'upper center', 'lower center', 'center left', 'center right': legend diletakkan di tepi axes 
+#       (atas tengah, bawah tengah, tengah kiri, tengah kanan)
 #     'center': legend diletakkan di tengah-tengah axes
 #     'best': matplotlib akan memilih satu dari sekian kemungkinan lokasi legend di atas yang paling tidak overlap dengan isi grafik
-# bbox_to_anchor: biasanya digunakan untuk adjust lokasi dari legend. Bisa berisi 2 angka 
+#   bbox_to_anchor: biasanya digunakan untuk adjust lokasi dari legend. Bisa berisi 2 angka 
 #                 yang menunjukkan koordinat x dan y (misal (1.6,0.5) berarti geser 1.6 ke kanan dan 0.5 ke atas). 
 #                 Bisa juga berisi 4 angka, angka ketiga dan keempat menyatakan width (lebar) dan height (tinggi) dari legend.
-# shadow: jika diisi True, maka kotak legend akan memiliki bayangan.
-# ncol: jumlah kolom dari isi legend, default-nya adalah 1
-# fontsize: ukuran huruf pada legend
+#   shadow: jika diisi True, maka kotak legend akan memiliki bayangan.
+#   ncol: jumlah kolom dari isi legend, default-nya adalah 1
+#   fontsize: ukuran huruf pada legend
 # title: memberikan judul pada legend
 # title_fontsize: ukuran huruf pada judul legend
