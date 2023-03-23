8bb209437fd54d8c8458da0b18e900134fbbc9d3
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 13:08:08 +0700

    Add script

--- Data Visualization with Python Matplotlib for Beginner/Part 2/case_6b.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/case_6b.py
@@ -9,11 +9,12 @@ top_brands = (dataset[dataset['order_month']=='2019-12'].groupby('brand')['quant
 dataset_top5brand_dec = dataset[(dataset['order_month']=='2019-12') & (dataset['brand'].isin(top_brands['brand'].to_list()))]
 plt.clf()
 #agregat per product
-data_per_product_top5brand_dec = dataset_top5brand_dec.groupby('product_id').agg({'quantity': 'sum', 'gmv':'sum', 'item_price':'median'}).reset_index()
+data_per_product_top5brand_dec = dataset_top5brand_dec.groupby('product_id').agg({'quantity': 'sum', 'gmv':'sum', 'item_price':'median'})
+.reset_index()
 #scatter plot
 plt.figure(figsize=(10,8))
 plt.scatter(data_per_product_top5brand_dec['item_price'],data_per_product_top5brand_dec['quantity'], marker='o', color='green')
-plt.title('Correlation of Quantity and GMV per Product\nTop 5 Brands in December 2019',fontsize=15, color='blue')
+plt.title('Correlation of Quantity and GMV per Product\nTop 5 Brands in December 2019',fontsize=15,color='blue')
 plt.xlabel('Price Median', fontsize = 12)
 plt.ylabel('Quantity',fontsize = 12)
 plt.xlim(xmin=0,xmax=2000000)
