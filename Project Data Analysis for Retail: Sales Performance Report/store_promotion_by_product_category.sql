d24e2d114e7707de6267090403c3592967515112
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 20:49:36 +0700

    Add script

--- Project Data Analysis for Retail: Sales Performance Report/store_promotion_by_product_category.sql
+++ Project Data Analysis for Retail: Sales Performance Report/store_promotion_by_product_category.sql
@@ -1,5 +1,17 @@
-select year(order_date) as years, product_sub_category, product_category, sum(sales) as sales, sum(discount_value) as promotion_value, round(sum(discount_value)/sum(sales)*100,2) as burn_rate_percentage
-from dqlab_sales_store
-where year(order_date) = '2012' and order_status = 'Order Finished'
-group by year(order_date), product_sub_category, product_category
-order by years, sales desc;
+select 
+  year(order_date) as years, 
+  product_sub_category, 
+  product_category, sum(sales) as sales, 
+  sum(discount_value) as promotion_value, 
+  round(sum(discount_value)/sum(sales)*100,2) as burn_rate_percentage
+from 
+  dqlab_sales_store
+where 
+  year(order_date) = '2012' and order_status = 'Order Finished'
+group by 
+  year(order_date), 
+  product_sub_category,
+  product_category
+order by 
+  years, 
+  sales desc;
