251b4006db0dd65f66af5273803dd47385f081cd
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 12:58:18 +0700

    Add script

--- Data Visualization with Python Matplotlib for Beginner/Part 2/2.line_chart_top_province.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 2/2.line_chart_top_province.py
@@ -1,17 +1,13 @@
 import datetime
 import pandas as pd
-
 dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
 dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
 dataset['gmv'] = dataset['item_price']*dataset['quantity']
-
 # Buat variabel untuk 5 propinsi dengan GMV tertinggi
 top_provinces = (dataset.groupby('province')['gmv'].sum().reset_index().sort_values(by='gmv', ascending=False).head(5))
 print(top_provinces)
-
 # Buat satu kolom lagi di dataset dengan nama province_top
 dataset['province_top'] = dataset['province'].apply(lambda x: x if (x in top_provinces['province'].to_list()) else 'other')
-
 # Plot multi-line chartnya
 import matplotlib.pyplot as plt
 dataset.groupby(['order_month', 'province_top'])['gmv'].sum().unstack().plot(marker='.', cmap='plasma')
