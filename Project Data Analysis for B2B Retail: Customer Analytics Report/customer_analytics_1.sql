5ef350e9be52ea73a2b223b6a70346cc0dc23f47
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 21:06:35 +0700

    Add script

--- Project Data Analysis for B2B Retail: Customer Analytics Report/customer_analytics_1.sql
+++ Project Data Analysis for B2B Retail: Customer Analytics Report/customer_analytics_1.sql
@@ -1,8 +1,11 @@
 select 
   quarter, count(distinct customerID) as total_customers
 from
-	(select customerID, createDate, quarter(createDate) as quarter
-	from customer
-	where createDate between '2004-01-01' and '2004-06-30') as tabel_b
+  (select
+    customerID, createDate, quarter(createDate) as quarter
+   from 
+    customer
+   where 
+    createDate between '2004-01-01' and '2004-06-30') as tabel_b
 group by 
   quarter;
