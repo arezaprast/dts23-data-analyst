020bae479d809d62fa13cdfd78fb79d250dbad95
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-03-23 12:48:29 +0700

    Add script

--- Data Visualization with Python Matplotlib for Beginner/Part 1/1.awal_dataset.py
+++ Data Visualization with Python Matplotlib for Beginner/Part 1/1.awal_dataset.py
@@ -2,4 +2,4 @@ import pandas as pd
 dataset = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced.csv')
 print('Ukuran dataset: %d baris dan %d kolom\n' % dataset.shape)
 print('Lima data teratas')
-print(dataset.head())
+print(dataset.head()) 
