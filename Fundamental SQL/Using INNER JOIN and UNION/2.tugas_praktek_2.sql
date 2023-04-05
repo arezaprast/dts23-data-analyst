27436bb1fc45769ef5b937e3eee36be62d7d4faa
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-04-05 17:23:46 +0700

    Add script

--- Fundamental SQL/Using INNER JOIN and UNION/2.tugas_praktek_2.sql
+++ Fundamental SQL/Using INNER JOIN and UNION/2.tugas_praktek_2.sql
@@ -1,1 +1,4 @@
-select * from tr_penjualan inner join ms_produk on tr_penjualan.kode_produk = ms_produk.kode_produk;
+select * 
+from tr_penjualan inner 
+join ms_produk 
+on tr_penjualan.kode_produk = ms_produk.kode_produk;
