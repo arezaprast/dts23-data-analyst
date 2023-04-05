63e18e8c704e54c26367c30a61381b281b056356
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-04-05 17:23:20 +0700

    Add script

--- Fundamental SQL/Using INNER JOIN and UNION/2.tugas_praktek_1.sql
+++ Fundamental SQL/Using INNER JOIN and UNION/2.tugas_praktek_1.sql
@@ -1,1 +1,4 @@
-select * from ms_item_warna inner join ms_item_kategori on ms_item_warna.nama_barang = ms_item_kategori.nama_item;
+select * 
+from ms_item_warna inner 
+join ms_item_kategori 
+on ms_item_warna.nama_barang = ms_item_kategori.nama_item;
