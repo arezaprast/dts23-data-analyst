8f23ff3ee50a564fded419995afb4bb1acec9558
Ahmad Reza Prasetia <prasahmad78@gmail.com>
2023-04-05 17:24:27 +0700

    Add script

--- Fundamental SQL/Using INNER JOIN and UNION/2.tugas_praktek_3.sql
+++ Fundamental SQL/Using INNER JOIN and UNION/2.tugas_praktek_3.sql
@@ -1,7 +1,8 @@
 SELECT 
   tr_penjualan.kode_transaksi, tr_penjualan.kode_pelanggan, 
-  tr_penjualan.kode_produk, ms_produk.nama_produk, ms_produk.harga, 
-  tr_penjualan.qty, ms_produk.harga*tr_penjualan.qty as total
+  tr_penjualan.kode_produk, ms_produk.nama_produk, 
+  ms_produk.harga, tr_penjualan.qty, 
+  ms_produk.harga*tr_penjualan.qty as total
 FROM 
   tr_penjualan
 INNER JOIN 
